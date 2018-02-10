52bfea309807f953970a13857ad3bbfbbc76453e
coleatkinson <coleatkinson@users.noreply.github.com>
2018-02-09 20:43:31 -0500

    Update Fallback-Rates-for-External-Carriers.rb

--- Shipping Scripts/Fallback Rates for External Carriers/Fallback-Rates-for-External-Carriers.rb
+++ Shipping Scripts/Fallback Rates for External Carriers/Fallback-Rates-for-External-Carriers.rb
@@ -1,13 +1,3 @@
-# This shipping script is a bit esoteric, but it's useful for high-volume stores
-# where even a few minutes of downtime on the part of a third party shipping
-# calcluation app could result in lost sales.
-#
-# This script assumes that you're using one or more third party carrier APIs for
-# fetching shipping rates (CARRIER_SOURCES), and that you've also set up some
-# "fallback" rates in Shopify as a backup. If any carrier rates are present, this
-# script will hide the fallback Shopify rates. If they're not (eg because the third
-# party is down or times out), the fallback rates remain.
-
 CARRIER_SOURCES = ['custom_app']
 
 carrier_rates_present = Input.shipping_rates.any? { |shipping_rate| CARRIER_SOURCES.include?(shipping_rate.source) }
