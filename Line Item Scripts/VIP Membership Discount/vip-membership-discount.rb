e17273609fe84ee609c452ad6c320189c8a04a26
coleatkinson <coleatkinson@users.noreply.github.com>
2018-02-16 18:37:23 -0500

    Update vip-membership-discount.rb

--- Line Item Scripts/VIP Membership Discount/vip-membership-discount.rb
+++ Line Item Scripts/VIP Membership Discount/vip-membership-discount.rb
@@ -3,10 +3,11 @@ DISCOUNT_AMOUNT = 10
 CUSTOMER_TAG = 'MEMBERSHIP'
 
 if !Input.cart.customer.nil? and Input.cart.customer.tags.include?(CUSTOMER_TAG)
-# Apply the discount to every item in the cart.
-Input.cart.line_items.each do |line_item|
-  line_item.change_line_price(line_item.line_price * (1.0 - (DISCOUNT_AMOUNT / 100.0)), message: "#{DISCOUNT_AMOUNT}% off store-wide!")
-end
+  # Apply the discount to every item in the cart.
+  Input.cart.line_items.each do |line_item|
+    next if line_item.variant.product.gift_card?
+      line_item.change_line_price(line_item.line_price * (1.0 - (DISCOUNT_AMOUNT / 100.0)), message: "#{DISCOUNT_AMOUNT}% off store-wide!")
+    end
 end
 
 Output.cart = Input.cart
